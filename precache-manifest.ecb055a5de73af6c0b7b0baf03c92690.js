0dd59070cbd2e9367dfec733789703e8b3b88537
priansukoirala <koiralapriansu@gmail.com>
2020-09-08 17:42:10 +0545

    Updates

---
+++ precache-manifest.ecb055a5de73af6c0b7b0baf03c92690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f006a5ade356ec5d0563e01e916a273",
+    "url": "/React-TodoApp/index.html"
+  },
+  {
+    "revision": "85ce96f5097619299b23",
+    "url": "/React-TodoApp/static/css/main.0d9c423f.chunk.css"
+  },
+  {
+    "revision": "bc32e9450d867891c176",
+    "url": "/React-TodoApp/static/js/2.4a4c7e4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-TodoApp/static/js/2.4a4c7e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ce96f5097619299b23",
+    "url": "/React-TodoApp/static/js/main.808365b3.chunk.js"
+  },
+  {
+    "revision": "8ce703a7b8299c94ffed",
+    "url": "/React-TodoApp/static/js/runtime-main.97b6aa0b.js"
+  }
+]);
